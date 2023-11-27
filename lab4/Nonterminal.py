1e2749da2c49076fe76a2237ae354e30d9d59bed
stepan <step.rus.f@gmail.com>
2023-11-27 15:59:45 +0300

    lab4

---
+++ lab4/Nonterminal.py
@@ -0,0 +1,139 @@
+from dataclasses import dataclass, field
+from logging import getLogger
+
+logger = getLogger('Nonterminal')
+
+
+@dataclass(frozen=True)
+class Token:
+    isNonTerminal: bool
+    text: str
+
+    def __lt__(self, other: 'Token'):
+        return self.text < other.text
+
+
+@dataclass
+class Rule:
+    rule: list[Token] = field(default_factory=list)
+    sync: bool = False
+
+    def __str__(self):
+        return ('Sync' if self.sync else '') + ''.join([
+            f"<{i.text}>" if i.isNonTerminal else f"'{i.text}'" for i in self.rule])
+
+    def __iter__(self):
+        for token in self.rule:
+            yield token
+
+
+class Nonterminal:
+
+    def __init__(self, name: str, rules: list[str], is_first=False) -> None:
+        self.is_first = is_first
+        self.name = name
+        self.rules = self._parse_rules(rules)
+        logger.debug(f"Created Nonterminal {name}. Rules are: {self.rules}")
+
+    def _parse_rules(self, rules: list[str]) -> list[Rule]:
+        res_rules = []
+        for rule in rules:
+            current_rule = []
+            is_processing_terminal = False
+            is_processing_nonterminal = False
+            current_token = ''
+            for char in rule:
+                if not (is_processing_terminal or is_processing_nonterminal):
+                    if char == '<':
+                        is_processing_nonterminal = True
+                        current_token =''
+                        continue
+                    elif char == '\'':
+                        is_processing_terminal = True
+                        current_token = ''
+                        continue
+                    elif char=='@':
+                        current_rule.append(Token(isNonTerminal=False, text='@'))
+                    elif char != ' ':
+                        raise ValueError(f'Unexpected token {char} in rule {rule} (Terminal {self.name})')
+                    continue
+                if is_processing_terminal:
+                    if char == '\'':
+                        is_processing_terminal = False
+                        current_rule.append(Token(isNonTerminal=False, text=current_token))
+                    else:
+                        current_token += char
+                        continue
+                if is_processing_nonterminal:
+                    if char == '>':
+                        is_processing_nonterminal = False
+                        current_rule.append(Token(isNonTerminal=True, text=current_token))
+                    else:
+                        current_token += char
+            res_rules.append(Rule(rule=current_rule))
+        return res_rules
+
+    @staticmethod
+    def first_for_rule(rule: Rule, other_nonterminals: dict[str, 'Nonterminal']) -> set[Token]:
+        rule = rule.rule
+        if not rule:
+            return {Token(isNonTerminal=False, text='@')}
+        result = set()
+        for i, token in enumerate(rule):
+            if token.isNonTerminal:
+                other_first = other_nonterminals[token.text].first(other_nonterminals)
+                if '@' not in other_first:
+                    result = result.union(other_first)
+                    break
+                else:
+                    if i == len(rule) - 1:  # If the last one returns @ have to add it to first.
+                        result = result.union(other_first)
+                        break
+                    else:  # Else try next one
+                        other_first.remove(Token(False, '@'))
+                        result = result.union(other_first)
+                        continue
+            else:
+                result.add(token)
+                break
+        return result
+
+    def first(self, other_rules: dict[str, 'Nonterminal']) -> set[Token]:
+        result = set()
+        for rule in self.rules:
+            result = result.union(self.first_for_rule(rule, other_rules))
+        return result
+
+    @property
+    def token(self) -> Token:
+        return Token(isNonTerminal=True, text=self.name)
+
+    def follow(self, other_nonterminals: dict[str, 'Nonterminal'], ignore:'Nonterminal'=None) -> set[Token]:
+        follow = set()
+        if self.is_first:
+            follow.add(Token(isNonTerminal=False, text='$'))
+        for nonterminal in other_nonterminals.values():
+            for rule in nonterminal.rules:
+                rule = rule.rule
+                last_token_position=0
+                for _ in range(rule.count(self.token)):
+                    last_token_position = rule.index(self.token, last_token_position)+1
+                    subrule = rule[last_token_position:]
+                    first_of_subrule = self.first_for_rule(Rule(subrule), other_nonterminals)
+                    if '@' not in [i.text for i in first_of_subrule]:
+                        follow = follow.union(first_of_subrule)
+                    else:
+                        if self == nonterminal and first_of_subrule == {Token(False, "@")}:
+                            continue
+                        follow=follow.union(first_of_subrule)
+                        if nonterminal!=ignore:
+                            follow = follow.union(nonterminal.follow(other_nonterminals, ignore=self))
+        return follow
+
+    def get_terminals(self) -> set[Token]:
+        terminals = set()
+        for rule in self.rules:
+            for token in rule.rule:
+                if not token.isNonTerminal:
+                    terminals.add(token)
+        return terminals
---
+++ lab4/Parser.py
@@ -0,0 +1,223 @@
+import typing
+from logging import getLogger
+from prettytable import PrettyTable
+
+from Nonterminal import Nonterminal, Token, Rule
+import re
+
+logger = getLogger('Parser')
+
+
+class Stack(list):
+    def append(self, __object) -> None:
+        logger.debug(f"stack {self}<-{__object}")
+        super().append(__object)
+
+    def pop(self, __index: typing.SupportsIndex = ...):
+        obj = super().pop()
+        logger.debug(f"stack {self}->{obj}")
+        return obj
+
+
+class Parser:
+    nonterminals: dict[str, Nonterminal]
+    NT_RULE = r"<(?P<nonterminal>[a-z_]+)>: *(?P<rules>.*)?"
+    NT_OR_RULE = r"\s*\| (?P<rules>.+)"
+    parsing_table: dict[Token, dict[Token, Rule]]
+    syntax_analysis_table: dict[Token, dict[Token, Rule]]
+
+    class Error(typing.NamedTuple):
+        message: str
+        position: int
+        skipped: bool
+
+    def __init__(self) -> None:
+        self.syntax_analysis_table = {}
+        self.parsing_table = {}
+        self.nonterminals = {}
+
+    @classmethod
+    def from_file(cls, filename):
+        instance = Parser()
+        with open(filename) as file:
+            current_ruleset: list[str] = []
+            current_nonterminal = None
+            is_first = True
+            for line in file.readlines():
+                new_nonterminal, rules = instance._parse_string(line)
+                if new_nonterminal:
+                    if current_nonterminal:
+                        instance.nonterminals[current_nonterminal] = Nonterminal(current_nonterminal, current_ruleset,
+                                                                                 is_first)
+                        if is_first:
+                            is_first = False
+                    current_nonterminal = new_nonterminal
+                    current_ruleset = rules.copy() if rules else []
+                else:
+                    current_ruleset += rules.copy()
+            instance.nonterminals[current_nonterminal] = Nonterminal(current_nonterminal, current_ruleset, is_first)
+        instance.__build_parsing_table()
+        return instance
+
+    def print_all_firsts(self):
+        for key, nonterminal in self.nonterminals.items():
+            logger.info(f"FIRST({key}) => {nonterminal.first(self.nonterminals)}")
+
+    def print_all_follows(self):
+        for key, nonterminal in self.nonterminals.items():
+            logger.info(f"FOLLOW({key}) => {nonterminal.follow(self.nonterminals)}")
+
+    def _parse_string(self, string: str) -> tuple[str | None, list[str] | None]:
+        """
+        Picks nonterminal and rules from string. Returns list of raw expressions
+        :param string:
+        :return:
+        """
+        found = re.match(self.NT_RULE, string)
+        if found:
+            rule_str = found.group('rules')
+            rules = rule_str.split('|') if rule_str else ['@']
+            return found.group('nonterminal'), rules
+        found = re.match(self.NT_OR_RULE, string)
+        if found:
+            return None, found.group('rules').split('|')
+        raise ValueError(f'cannot parse string {string}')
+
+    def __build_parsing_table(self) -> None:
+        table: dict[Token, dict[Token, Rule]] = {}  # [nonterminal][terminal]
+        for nonterminal in self.nonterminals.values():
+            table[nonterminal.token] = {}
+            for rule in nonterminal.rules:
+                first = Nonterminal.first_for_rule(rule, self.nonterminals)
+                if '@' not in [i.text for i in first]:
+                    for terminal in first:
+                        table[nonterminal.token][terminal] = rule
+                else:
+                    for terminal in first:
+                        if terminal.text != '@':
+                            table[nonterminal.token][terminal] = rule
+                    for terminal in nonterminal.follow(self.nonterminals):
+                        logger.debug(f'table[{nonterminal.token.text}][{terminal.text}]={rule}')
+                        table[nonterminal.token][terminal] = rule
+
+        self.parsing_table = table
+
+    @property
+    def terminals(self):
+        terminals = set()
+        for i in self.nonterminals.values():
+            terminals = terminals.union(i.get_terminals())
+        return terminals
+
+    def print_table(self, table: dict[Token, dict[Token, Rule]], save_to_csv: str = None):
+        terminals = sorted(list(self.terminals))
+        pt = PrettyTable()
+        pt.field_names = [""] + [terminal.text for terminal in terminals]
+        for nonterminal in table:
+            row = [f"<{nonterminal.text}>"] + [
+                str(table[nonterminal].get(terminal)) for terminal in terminals
+            ]
+            pt.add_row(row)
+        print(pt)
+        if save_to_csv:
+            with open(save_to_csv, 'w', newline='') as file:
+                file.write(pt.get_csv_string())
+
+    def add_sync(self):
+        self.syntax_analysis_table = self.parsing_table.copy()
+        for nonterminal in self.nonterminals.values():
+            for terminal in nonterminal.follow(self.nonterminals):
+                current_rule = self.syntax_analysis_table[nonterminal.token].get(terminal)
+                if current_rule:
+                    self.syntax_analysis_table[nonterminal.token][terminal].sync = True
+                else:
+                    self.syntax_analysis_table[nonterminal.token][terminal] = Rule(sync=True)
+            for terminal in nonterminal.first(self.nonterminals):
+                if terminal.text == '@':
+                    continue
+                self.syntax_analysis_table[nonterminal.token][terminal].sync = True
+
+    def test_string(self, string: str) -> list[Error]:
+        """
+        Parses string, returns errors
+        """
+        stack = Stack([Token(False, '$'), Token(True, 'program')])
+        errors = []
+        position = 0
+        in_panic = False
+
+        def panic_mode():
+            nonlocal position, in_panic
+            while position <= len(string) and in_panic:
+                errors.append(self.Error('', position, True))
+                while string[position] in (' ', '\n') and position < len(string):
+                    position += 1
+                next_terminal = self._find_next_terminal(string[position:])
+                new_rule = self.syntax_analysis_table[stack_element].get(next_terminal)
+                if new_rule:
+                    if new_rule.sync:
+                        position += 1
+                        in_panic = False
+                position += 1
+
+        while position < len(string):
+            next_terminal = None
+            try:
+                while string[position] in (' ', '\n') and position < len(string) - 1:
+                    position += 1
+                next_terminal = self._find_next_terminal(string[position:])
+            except ValueError:
+                errors.append(self.Error('Not found terminal', position, False))
+                position += 1
+                continue
+            stack_element = stack.pop()
+            if stack_element.isNonTerminal:
+                new_rule = self.syntax_analysis_table[stack_element].get(next_terminal)
+                if new_rule:
+                    if new_rule.rule:
+                        for token in new_rule.rule[::-1]:
+                            stack.append(token)
+                        continue
+                    else:
+                        assert new_rule.sync
+                        errors.append(
+                            self.Error(f'Next rule not found for {stack_element.text}[{next_terminal.text}] (synch)',
+                                       position, False))
+                        stack.pop()
+                        position += 1
+                        continue
+                else:
+                    in_panic = True
+                    errors.append(
+                        self.Error(f'Next rule not found for {stack_element.text}[{next_terminal.text}]', position,
+                                   False))
+                    position += 1
+                    panic_mode()
+                    continue
+            else:
+                if stack_element == next_terminal:
+                    position += len(next_terminal.text)
+                elif stack_element == Token(False, "@"):
+                    continue
+                else:
+                    errors.append(
+                        self.Error(f'Unexpected symbol (expected {stack_element.text} got {string[position]})',
+                                   position,
+                                   False))
+                    in_panic = True
+                    position += 1
+                    while not stack_element.isNonTerminal:
+                        if not stack:
+                            errors += [self.Error('', i, True) for i in range(position, len(string))]
+                            return errors
+                        stack_element = stack.pop()
+                    panic_mode()
+                    continue
+        errors += [self.Error(f"Expected {i.text}", -1, False) for i in stack[1:][::-1]]
+        return errors
+
+    def _find_next_terminal(self, string: str) -> Token:
+        for terminal in sorted(list(self.terminals), key=lambda token: len(token.text), reverse=True):
+            if string.startswith(terminal.text):
+                return terminal
+        raise ValueError(f'Unable to find next terminal in {string.__repr__()}')
